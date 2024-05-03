175e5a31340972de16f0f0c4c9aca4b96b75760d
souzannatha <souzanatha04@gmail.com>
2024-05-03 14:40:47 -0300

    feast: add header and fonts.

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,11 +1,18 @@
 import { View, Text } from "react-native";
 import { styles } from "./styles";
+import { Ingredient } from "@/components/Ingredients";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Escolha os produtos</Text>
-      <Text>Descubra receitas baseadas nos produtos que você escolheu.</Text>
+      <Text style={styles.title}>
+        Escolha{"\n"}
+        <Text style={styles.subtitle}>os produtos</Text>
+      </Text>
+      <Text style={styles.message}>
+        Descubra receitas baseadas nos produtos que você escolheu.
+      </Text>
+      <Ingredient />
     </View>
   );
 }
